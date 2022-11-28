60249b05345a6dd6ba76316a15269a4add577a9b
HankC138 <corbincampbell1990@gmail.com>
2022-11-27 19:45:36 -0500

    removed comments that shouldnt be there

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
-const twilio = require("twilio")(process.env.ACCOUNTSID,process.env.AUTHTOKEN);
+const twilio = require("twilio")(process.env.ACCOUNTSID, process.env.AUTHTOKEN);
 const router = express.Router();
 const { signup, factorview } = require("../views/");
 const { User, Code } = require("../models");
@@ -29,20 +29,14 @@ router.post("/", async (req, res, next) => {
 			valid_until: validTime,
 		});
 		await code.setUser(user);
-	//numbers as an environment variable
-	const fromNum = process.env.FROMNUM;
-	const toNum = user.phone;
-	//bodt as environment variable
-	const messageBody = ("your 2 factor authentication code is " + code.authCode);
-	//requiring twilio package
-
-	//individual message from twilio
-	await twilio.messages.create({
-		body: messageBody,
-		//numbers as an environment variable
-		from: fromNum,
-		to: toNum,
-	});
+		const fromNum = process.env.FROMNUM;
+		const toNum = user.phone;
+		const messageBody = "your 2 factor authentication code is " + code.authCode;
+		await twilio.messages.create({
+			body: messageBody,
+			from: fromNum,
+			to: toNum,
+		});
 		await res.redirect(`/auth/${user.id}`);
 	} catch (error) {
 		next(error);
